5e82846679f781cf3cbcc21a5746b880734752dd
Curtis Maloney <curtis@tinbrain.net>
2018-06-17 15:30:39 +1000

    Fix declaration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Heavily inspired by csv-exportor
  */
 
-export downlink(content, filename, type = 'text/plain; charset=utf-8') {
+export function downlink(content, filename, type = 'text/plain; charset=utf-8') {
 	const blob = new Blob(content, {type});
 	const url = window.URL.createObjectURL(blob);
 
